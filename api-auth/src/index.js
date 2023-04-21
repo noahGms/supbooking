2c877160d678f301e81624b2226a1473b2d2ad1d
noahGms <noah@keshin.net>
2023-04-21 20:53:05 +0200

    setup minimal express config with mongoose in api-auth

--- api-auth/src/index.js
+++ api-auth/src/index.js
@@ -1,7 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import {setupDatabase} from "./config/db.js";
 
 (async () => {
   const app = express();
+
+  app.use(morgan('dev'));
+  app.use(helmet());
+
+  await setupDatabase();
 
   app.get('/', (req, res) => {
     res.send('Hello World from API-AUTH !');
